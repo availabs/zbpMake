f6eb4f56fc814e022023ee9fbad770b2c825d1bc
cakesofwrath <dai.alan.dai@gmail.com>
2015-06-02 12:15:49 -0400

    fixez

--- dataProcessing/zbpConverter.py
+++ dataProcessing/zbpConverter.py
@@ -22,18 +22,20 @@ tabuls = {
 #key = {yr: keyYear for yr in keyYear for keyYear in tabuls}
 key = {}
 for key_yr in tabuls:
-	for yr in key_yr:
+	for yr in tabuls[key_yr]:
 		key[yr] = key_yr
 
 def not_in(s, yr1, yr2):
-	print "%s not present from %s to %s" % (s, yr1, yr2)
+	# print "%s not present from %s to %s" % (s, yr1, yr2)
 	return ""
 
 def not_in_data(dn):
-	print "%s not in concord" % (dn)
+	# print "%s not in concord" % (dn)
 	return dn
 
 def get_next_yr(yr):
+	if yr in tabuls:
+		return get_next_yr(str(int(yr) + 1))
 	if yr in tabuls["1987"]:
 		return "1997"
 	elif yr in tabuls["1997"]:
@@ -47,34 +49,38 @@ def get_next_yr(yr):
 
 # somewhat inefficient, but works
 def convert(file_year, year_to="2012"):
-	year, year_goal = key[file_year], key[year_to] # the classification year that each year falls under
-	data = pd.read_csv("raw/zbp%sdetail.txt" % str(file_year)[2:]) 
+	year, year_goal = key[str(file_year)], key[year_to] # the classification year that each year falls under
+	data = pd.read_csv("raw/zbp%sdetail.txt" % str(file_year)[2:], dtype={"zip": "object"}) 
 	first = True # b/c first time concord is different
-	while year != yearGoal:
-		concord_raw = pd.read_csv("%s_%s.csv" % (year, convs[year]), "r")
-		keys = list(concord.columns.values) # get column keys
+	while year != year_goal:
+		concord_raw = pd.read_csv("../concordances/%s_%s.csv" % (year, convs[year]))
+		keys = list(concord_raw.columns.values) # get column keys
 		if first:
-			concord = {l[keys[0]]: l[keys[2]] for l in concord_raw.values}
+			concord = {l[0]: l[2] for l in concord_raw.values}
 			first = False
 		else:
-			this_conc = {l[keys[0]]: l[keys[2]] for l in concord.values}
-			concord = {k: this_conc[k] if k in this_conc else not_in(k, year, yearGoal) for k in concord} # converts over
+			this_conc = {l[0]: l[2] for l in concord_raw.values}
+			concord = {k: this_conc[k] if k in this_conc else not_in(k, year, year_goal) for k in concord} # converts over
 		year = get_next_yr(year)
-	data["naics"] = pd.Series([concord[l] if l in concord else not_in_data(l) for l in data["naics"]])
-	pd.to_csv("converted/zbp%sdetail_conv.txt" % str(file_year)[2:])
+		print year
+	data[data.columns.values[2]] = pd.Series([concord[l] if l in concord else not_in_data(l) for l in data[data.columns.values[2]]])
+	data.columns.values[2] = "naics"
+	data.to_csv("converted/zbp%sdetail_conv.txt" % str(file_year)[2:], index=False)
 
 if __name__ == "__main__":
-	if len(sys.argv) != 5 or len(sys.argv) != 3:
-		print "Usage: python zbpConverter.py <two-digit year> [-y] [yearToConvertTo]"
+	if len(sys.argv) != 4 and len(sys.argv) != 2:
+		print "Usage: python zbpConverter.py <four-digit year> [-y] [yearToConvertTo]"
 		sys.exit()
-	elif not sys.argv[2].isnumeric() or (int(sys.argv[2]) < 0 or (int(sys,argv[2]) > 13 and not int(sys.argv[2]) < 94) or int(sys.argv[2]) > 100):
+	year = int(sys.argv[1]) if unicode(sys.argv[1], "utf-8").isnumeric() else -1
+	if not (1993 < year < 2014):
 		print "Invalid year"
 		sys.exit()
-	year = int(sys.argv[2])
-	if len(sys.argv) == 5 and sys.argv[3] == '-y':
-		if not sys.argv[4].isnumeric() or (int(sys.argv[3]) < 0 or (int(sys,argv[3]) > 13 and not int(sys.argv[3]) < 94) or int(sys.argv[3]) > 100):
+
+	if len(sys.argv) == 4 and sys.argv[2] == '-y':
+		year_to = int(sys.argv[3]) if unicode(sys.argv[3], "utf-8").isnumeric() else -1
+		if not (1993 < year_to < 2014):
 			print "Invalid year to convert to"
 			sys.exit()
-		convert(year, yearTo=int(sys.argv[4]))
+		convert(year, year_to=int(sys.argv[4]))
 	else:
 		convert(year)
